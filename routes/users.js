d84553c9db36faecce8be0ea8614f672f947849e
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-09-12 16:56:17 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('All Users Retrieved : Take 3');
+  res.send('All Users Retrieved : Take 4');
 });
 
 module.exports = router;
